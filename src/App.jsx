9817ac7147faa88a2f785abee7ab8291bc172772
sofowest <leandrogomezmdev@gmail.com>
2025-08-16 13:06:27 -0300

    Add Header component and update main layout
    Introduced a new Header component with a fixed top bar and added it to App.jsx. Updated the main layout with two styled sections and added a primary color logo image. Created an empty HomePage.jsx for future content.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import './global.css'
+import Header from './components/layout/Header.jsx'
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold center blue">hola mundo</h1>
+      <Header />
+      <section className="h-dvh flex items-center justify-center bg-amber-100">
+        contenedor fait
+      </section>
+      <section className="h-dvh flex items-center justify-center bg-amber-300">
+        contenedor fait 2
+      </section>
     </>
   )
 }
---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <>
+      <header className="bg-blue-500 text-white p-4 fixed top-0 w-full ">
+        <h1>luzyvidafait</h1>
+      </header>
+    </>
+  )
+}
+
+export default Header
