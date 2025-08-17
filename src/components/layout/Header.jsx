498efb20e38ad6f1a34d4b3f3b6aa8647a04d10e
sofowest <leandrogomezmdev@gmail.com>
2025-08-17 05:01:58 -0300

    Update images and UI styles on homepage and header
    Added new webp images to the public/imgs/auxiliar directory and updated the isologo.svg. Modified Header.jsx, global.css, and HomePage.jsx to reflect new assets and adjust styles or layout as needed.

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -3,17 +3,17 @@ import React from 'react'
 function Header() {
   return (
     <>
-      <header className="bg-[var(--background-opacity-color)] backdrop-blur-sm text-white fixed top-0 w-full h-18 flex m-0 p-0 flex-col z-50">
+      <header className="bg-[var(--background-opacity-color)] backdrop-blur-sm text-white fixed top-0 w-[100vw] h-18 flex m-0 p-0 flex-col z-50">
         <div
           id='notch'
           className='fixed left-1/2 -translate-x-1/2 top-0 flex items-center justify-center'
         >
           <div className='bg-[var(--primary-color)] w-70 h-[10px] rounded-b-[15px] z-55'></div>
         </div>
-        <section className='flex mx-2 justify-between items-center h-full pt-3'>
-            <img src="/imgs/isologo.svg" alt="Isologo de Luz y Vida" className='w-20'/>
-            <h1 className='text-[var(--primary-color)] font-black'>Luz Y Vida</h1>
-            <img src="/imgs/menu.svg" alt="Icono de Menu" className='w-15'/>
+        <section className='flex mx-3 justify-between items-center h-full pt-3'>
+          <img src="/imgs/isologo.svg" alt="Isologo de Luz y Vida" className="w-15" />
+          <h1 className='text-[var(--primary-color)] font-bold'>LUZ Y VIDA</h1>
+          <img src="/imgs/menu.svg" alt="Icono de Menu" className='w-15'/>
         </section>
       </header>
     </>
