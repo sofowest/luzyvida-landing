504f75aea00d11946e1862e8b1e05c9d0e51ef4e
sofowest <leandrogomezmdev@gmail.com>
2025-08-20 11:37:37 -0300

    media queries lg for HomePage first impression
    Updated HomePage layout and styles for better responsiveness on large screens, including flexbox adjustments and enhanced grid image presentation. Adjusted DesktopNavbar spacing and font sizing for improved appearance on larger devices.

--- src/components/ui/DesktopNavbar.jsx
+++ src/components/ui/DesktopNavbar.jsx
@@ -2,12 +2,12 @@ import { Link } from 'react-router-dom';
 
 function DesktopNavbar() {
   return (
-    <nav className="flex mx-10 justify-between items-center h-full text-white text-xl font-light">
+    <nav className="flex mx-10 justify-between items-center h-full text-white font-light lg:text-xl">
       <section className="flex items-center space-x-8">
         <Link to="/"><img src="/imgs/isologo-white.svg" alt="Isologo de Luz y Vida" className="w-16 h-16" /></Link>
         <h1 className='text-white font-bold font-(family-name:--font-rubik)'>LUZ Y VIDA</h1>
       </section>
-      <div className="flex space-x-20">
+      <div className="flex space-x-20 lg:space-x-10">
         <Link to="/" className="hover:text-[var(--secondary-color)]">Inicio</Link>
         <Link to="/servicios" className="hover:text-[var(--secondary-color)]">Servicios</Link>
         <Link to="/contacto" className="hover:text-[var(--secondary-color)]">Nuestro Equipo</Link>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,29 +1,31 @@
 function HomePage() {
   return (
     <>
-      <article className="h-dvh flex flex-col">
-          <section className='pt-30 px-5 text-[var(--primary-color)] h-80 mb-8 md:flex-col'>
-              <p className="mt-4 text-md">Más que un residencial</p>
-              <h1 className="text-4xl font-bold py-0">Un hogar hecho con mucho amor</h1>
-              <h2 className='text-md w-75'>Donde el cuidado se convierte <br /> en calidad de vida</h2>
+      <article className="h-dvh lg:flex lg:pt-[10vh]">
+        <div className="flex flex-col lg:h-full lg:w-[50vw] lg:justify-center lg:items-center lg:px-5">
+          <section className='pt-30 px-5 text-[var(--primary-color)] h-80 mb-8 lg:w-full lg:h-[50vh] lg:flex-col lg:pt-0 lg:px-0 lg:mb-0'>
+              <p className="mt-4 text-md lg:text-2xl lg:pb-3">Más que un residencial</p>
+              <h1 className="text-[2.1rem] font-(family-name:--font-rubik) font-bold py-0 lg:text-[2.8rem]">Un hogar hecho con <br /> mucho amor</h1>
+              <h2 className='text-md w-75 lg:w-full lg:text-2xl'>Donde el cuidado se convierte <br /> en calidad de vida</h2>
           </section>
-          <section className="items-center justify-center flex h-[10vh] md:flex-col">
-            <button className='bg-[var(--primary-color)] block px-8 py-4 rounded-xl text-[var(--secondary-color)] w-90 font-medium text-2xl'>¡CONTÁCTANOS!</button>
+          <section className="items-center justify-center flex h-[10vh] lg:flex-col lg:w-full lg:bottom-0">
+            <button className='bg-[var(--primary-color)] block px-8 py-4 rounded-xl text-[var(--secondary-color)] w-90 font-bold text-2xl lg:flex-col lg:w-full lg:rounded-2xl lg:text-2xl lg:h-full'>¡CONTÁCTANOS!</button>
           </section>
-          <section className="w-full h-[40vh] flex items-center justify-center px-5 my-10 md:flex-row">
-              <div className="w-full h-full grid grid-cols-3 md:grid-cols-5 grid-rows-2 md:grid-rows-4 gap-2 md:gap-2">
-                <div className="col-start-1 row-start-1 col-span-2 bg-gray-300 rounded-md h-full
-                                md:col-start-1 md:row-start-1 md:col-span-3 md:row-span-2 ">
-                  <img src="/imgs/vento-img-inicio-1.webp" alt="Empleadas de Luz Y Vida posando para una foto en conjunto" className="w-full h-full object-cover rounded-md"/>
-                </div>
-                <div className="col-start-1 row-start-2 col-span-2 md:col-start-1 md:row-start-3 md:col-span-3 md:row-span-2 bg-gray-300 rounded-md h-full">
-                  <img src="/imgs/vento-img-inicio-2.webp" alt="Mano de una persona mayor con un oxímetro de pulso en el dedo" className="w-full h-full object-cover rounded-md"/>
-                </div>
-                <div className="col-start-3 row-start-1 row-span-2 md:col-start-4 md:row-start-1 md:col-span-2 md:row-span-4 bg-gray-300 rounded-md h-full">
-                  <img src="/imgs/vento-img-inicio-3.webp" alt="Fachada de las instalaciones de Luz Y Vida" className="w-full h-full object-cover rounded-md"/>
-                </div>
+        </div>
+        <section className="w-full h-[40vh] flex items-center justify-center px-5 my-10 lg:h-full lg:w-[50vw] lg:justify-center lg:items-center lg:m-0">
+          <div className="w-full h-full grid grid-cols-3 md:grid-cols-5 grid-rows-2 md:grid-rows-4 gap-2 md:gap-2 lg:h-[60vh] lg:w-[50vw] lg:gap-5">
+            <div className="col-start-1 row-start-1 col-span-2 bg-gray-300 rounded-md h-full
+                            md:col-start-1 md:row-start-1 md:col-span-3 md:row-span-2 lg:rounded-2xl">
+              <img src="/imgs/vento-img-inicio-1.webp" alt="Empleadas de Luz Y Vida posando para una foto en conjunto" className="w-full h-full object-cover rounded-md lg:rounded-2xl"/>
             </div>
-          </section>
+            <div className="col-start-1 row-start-2 col-span-2 md:col-start-1 md:row-start-3 md:col-span-3 md:row-span-2 bg-gray-300 rounded-md h-full lg:rounded-2xl">
+              <img src="/imgs/vento-img-inicio-2.webp" alt="Mano de una persona mayor con un oxímetro de pulso en el dedo" className="w-full h-full object-cover rounded-md lg:rounded-2xl"/>
+            </div>
+            <div className="col-start-3 row-start-1 row-span-2 md:col-start-4 md:row-start-1 md:col-span-2 md:row-span-4 bg-gray-300 rounded-md h-full lg:rounded-2xl">
+              <img src="/imgs/vento-img-inicio-3.webp" alt="Fachada de las instalaciones de Luz Y Vida" className="w-full h-full object-cover rounded-md lg:rounded-2xl"/>
+            </div>
+          </div>
+        </section>
       </article>
 
       <article className="h-dvh flex items-center justify-center bg-red-200">
