48311c12b90e41628a573a1b18c8a12c8dddba5f
sofowest <leandrogomezmdev@gmail.com>
2025-08-20 09:37:20 -0300

    Refactor image assets and update HomePage references
    Moved and renamed image assets from 'public/imgs/auxiliar/' to 'public/imgs/', removing unused jpg files and updating HomePage.jsx to reference the new image paths with descriptive alt text. Minor header and config cleanup for improved layout and maintainability.

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -7,15 +7,10 @@ function Header() {
   const windowWidth = useWindowWidth();
   const isMobile = windowWidth <= 768;
 
-//   const location = useLocation();
-
-//   if (location.pathname === '/admin') {
-//     return null; // Opcional: No mostrar el header en la página de administración
-//   }
-
   return (
-    <header className="bg-[var(--background-opacity-color)] backdrop-blur-sm text-white fixed top-0 w-[100vw] h-18 flex m-0 p-0 flex-col z-50 
-                       md:bg-[var(--primary-color)] md:backdrop-blur-none md- md:h-20
+    <header className="bg-[var(--background-opacity-color)] backdrop-blur-sm text-white fixed top-0 w-[100vw] h-18 flex m-0 p-0 flex-col z-50
+                       md:bg-[var(--primary-color)] md:backdrop-blur-none md:h-20 md:w-full
+                       lg:bg-[var(--primary-color)] lg:backdrop-blur-none lg:h-20
     ">
         <div id='notch' className='fixed left-1/2 -translate-x-1/2 top-0 flex items-center justify-center w-70 h-[10px] md:hidden'>
           <div className='bg-[var(--primary-color)] w-full h-full rounded-b-[15px] z-55'></div>
