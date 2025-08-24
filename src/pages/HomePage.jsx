24e68359f96e564061a99c84191c340c76657389
sofowest <leandrogomezmdev@gmail.com>
2025-08-24 11:21:46 -0300

    images for Services

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -14,18 +14,18 @@ function HomePage() {
 
   return (
     <>
-      <article id="article-homepage" className="h-dvh lg:flex lg:pt-[10vh]">
-        <div className="flex flex-col h-[50vh] lg:h-full lg:w-[50vw] lg:justify-center lg:items-center lg:px-5">
-          <section className='h-[40vh] items-center justify-center text-balance pt-30 px-5 text-[var(--primary-color)] mb-8 relative lg:w-full lg:h-[50vh] lg:flex-col lg:pt-0 lg:px-0 lg:mb-0'>
+      <article id="article-homepage" className="h-vh pt-30 lg:flex lg:pt-[10vh]">
+        <div className="flex flex-col h-vh lg:h-full lg:w-[50vw] lg:justify-center lg:items-center lg:px-5">
+          <section className='h-[200px] items-center justify-center text-balance px-5 text-[var(--primary-color)] mb-8 relative lg:w-full lg:h-[50vh] lg:flex-col lg:pt-0 lg:px-0 lg:mb-0'>
               <p className="py-[1vh] text-lg lg:text-2xl lg:pb-3">Más que un residencial</p>
-              <h1 className="text-[2.1rem] py-[1vh] leading-10 font-(family-name:--font-rubik) font-bold lg:text-[2.8rem] lg:leading-12 lg:relative lg:inset-y-[5%]">Un hogar hecho con mucho amor</h1>
-              <h2 className="text-lg w-[75vw] py-[1vh] lg:absolute lg:bottom-[5%] lg:w-full lg:text-2xl">Donde el cuidado se convierte <br className="lg:hidden"/> en calidad de vida</h2>
+              <h1 className="text-[2.1rem] py-3 leading-10 font-(family-name:--font-rubik) font-bold lg:text-[2.8rem] lg:leading-12 lg:relative lg:inset-y-[5%]">Un hogar hecho con mucho amor</h1>
+              <h2 className="text-lg w-[75vw] py-3 lg:absolute lg:bottom-[5%] lg:w-full lg:text-2xl">Donde el cuidado se convierte <br className="lg:hidden"/> en calidad de vida</h2>
           </section>
-          <section className="items-center justify-center flex h-[10vh] lg:flex-col lg:w-full lg:bottom-0 pt-[1vh]">
+          <section className="items-center justify-center flex h-28 lg:flex-col lg:w-full lg:bottom-0 pt-3">
             <button className="bg-[var(--primary-color)] block px-8 py-3 rounded-xl text-[var(--secondary-color)] font-bold text-2xl hover:scale-110 hover:bg-[#810081] transition-all lg:flex-col lg:w-full lg:rounded-2xl lg:text-2xl lg:h-full">¡CONTÁCTANOS!</button>
           </section>
         </div>
-        <section className="w-full h-[40vh] flex items-center justify-center px-5 my-10 lg:h-full lg:w-[50vw] lg:m-0">
+        <section className="w-full h-[300px] flex items-center justify-center px-5 my-10 lg:h-full lg:w-[50vw] lg:m-0">
           <div className="w-full h-full grid grid-cols-3 md:grid-cols-5 grid-rows-2 md:grid-rows-4 gap-2 md:gap-2 lg:h-[60vh] lg:w-[50vw] lg:gap-5">
             <div className="col-start-1 row-start-1 col-span-2 bg-gray-300 rounded-md h-full
                             md:col-start-1 md:row-start-1 md:col-span-3 md:row-span-2 lg:rounded-2xl">
@@ -53,7 +53,7 @@ function HomePage() {
           <h2 className="text-5xl font-bold font-(family-name:--font-rubik) text-[var(--primary-color)] h-[10dvh]">Servicios</h2>
           <div className="grid grid-cols-1 gap-5 m-4 grid-rows-10 md:grid-cols-3 md:grid-rows-6 md:gap-10">
             <div className="w-[80vw] h-[40vh] col-start-1 row-start-1 row-span-2 md:w-[20vw] md:h-[20vw] md:col-start-1 md:row-start-1 md:col-span-1 md:row-span-3 bg-gray-300 rounded-md relative">
-              <img src="/imgs/services-2.webp" alt="Empleadas de Luz Y Vida posando para una foto en conjunto" className="w-full h-full object-cover rounded-md"/>
+              <img src="/imgs/services-1.webp" alt="Empleadas de Luz Y Vida posando para una foto en conjunto" className="w-full h-full object-cover rounded-md"/>
               <section className="absolute bottom-0 h-[20%] bg-[#7b007bc0] bg-opacity-70 w-full text-center text-[var(--secondary-color)] rounded-b-md flex items-center justify-center">
                 <h3 className="text-lg font-bold w-50">Seguimiento médico personalizado</h3>
               </section>
@@ -67,11 +67,11 @@ function HomePage() {
             <div className="w-[80vw] h-[40vh] col-start-1 row-start-5 row-span-2 md:w-[20vw] md:h-[20vw] md:col-start-3 md:row-start-1 md:col-span-1 md:row-span-3 bg-gray-300 rounded-md relative">
               <img src="/imgs/services-3.webp" alt="Empleadas de Luz Y Vida posando para una foto en conjunto" className="w-full h-full object-cover rounded-md"/>
               <section className="absolute bottom-0 h-[20%] bg-[#7b007bc0] bg-opacity-70 w-full text-center text-[var(--secondary-color)] rounded-b-md flex items-center justify-center">
-                <h3 className="text-lg font-bold w-50">Especialistas en cocina</h3>
+                <h3 className="text-lg font-bold w-50">Especialistas<br /> en cocina</h3>
               </section>
             </div>
             <div className="w-[80vw] h-[40vh] col-start-1 row-start-7 row-span-2 md:w-[20vw] md:h-[20vw] md:col-start-1 md:row-start-4 md:col-span-1 md:row-span-3 bg-gray-300 rounded-md relative">
-              <img src="/imgs/vento-img-inicio-1.webp" alt="Empleadas de Luz Y Vida posando para una foto en conjunto" className="w-full h-full object-cover rounded-md"/>
+              <img src="/imgs/services-4.webp" alt="Empleadas de Luz Y Vida posando para una foto en conjunto" className="w-full h-full object-cover rounded-md"/>
               <section className="absolute bottom-0 h-[20%] bg-[#7b007bc0] bg-opacity-70 w-full text-center text-[var(--secondary-color)] rounded-b-md flex items-center justify-center">
                 <h3 className="text-lg font-bold w-50">Peluquería</h3>
               </section>
@@ -79,11 +79,11 @@ function HomePage() {
             <div className="w-[80vw] h-[40vh] col-start-1 row-start-9 row-span-2 md:w-[20vw] md:h-[20vw] md:col-start-2 md:row-start-4 md:col-span-1 md:row-span-3 bg-gray-300 rounded-md relative">
               <img src="/imgs/vento-img-inicio-1.webp" alt="Empleadas de Luz Y Vida posando para una foto en conjunto" className="w-full h-full object-cover rounded-md"/>
               <section className="absolute bottom-0 h-[20%] bg-[#7b007bc0] bg-opacity-70 w-full text-center text-[var(--secondary-color)] rounded-b-md flex items-center justify-center">
-                <h3 className="text-lg font-bold w-50">Celebración de días festivos</h3>
+                <h3 className="text-lg font-bold w-50">Celebración<br /> de días festivos</h3>
               </section>
             </div>
             <div className="w-[80vw] h-[40vh] col-start-1 row-start-11 row-span-2 md:w-[20vw] md:h-[20vw] md:col-start-3 md:row-start-4 md:col-span-1 md:row-span-3 bg-gray-300 rounded-md relative">
-              <img src="/imgs/vento-img-inicio-1.webp" alt="Empleadas de Luz Y Vida posando para una foto en conjunto" className="w-full h-full object-cover rounded-md"/>
+              <img src="/imgs/services-6.webp" alt="Empleadas de Luz Y Vida posando para una foto en conjunto" className="w-full h-full object-cover rounded-md"/>
               <section className="absolute bottom-0 h-[20%] bg-[#7b007bc0] bg-opacity-70 w-full text-center text-[var(--secondary-color)] rounded-b-md flex items-center justify-center">
                 <h3 className="text-lg font-bold w-50">Actividades recreativas</h3>
               </section>
